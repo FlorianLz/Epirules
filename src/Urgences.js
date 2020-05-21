3a775a9d7ebd9402115eab0031fd5de97aaec80a
LydieV <49896819+LydieV@users.noreply.github.com>
2020-05-21 10:23:11 +0200

    Page Urgences.

--- src/Urgences.js
+++ src/Urgences.js
@@ -5,6 +5,63 @@ export default function Urgences(props) {
     return (
         <div>
             <Header page={'Urgences'}></Header>
+            <div className="emergency">
+                <div className="partEmergency">
+                    <div className="numero">
+                        <a href="tel:+15">
+                            <p> 15 </p>
+                        </a>
+                    </div>
+                    <div className="nameNumero">
+                        <p> SAMU </p>
+                    </div>
+                </div>
+
+                <div className="partEmergency">
+                    <div className="numero">
+                        <a href="tel:+17">
+                            <p> 17 </p>
+                        </a>
+                    </div>
+                    <div className="nameNumero">
+                        <p> Police </p>
+                    </div>
+                </div>
+
+                <div className="partEmergency">
+                    <div className="numero">
+                        <a href="tel:+18">
+                            <p> 18 </p>
+                        </a>
+                    </div>
+                    <div className="nameNumero">
+                        <p> Pompier </p>
+                    </div>
+                </div>
+
+                <div className="partEmergency">
+                    <div className="numero">
+                        <a href="tel:+112">
+                            <p> 112 </p>
+                        </a>
+                    </div>
+                    <div className="nameNumero">
+                        <p> Numéro Européen </p>
+                    </div>
+                </div>
+
+                <div className="partEmergency">
+                    <div className="numero">
+                        <a href="tel:+144">
+                            <p> 144 </p>
+                        </a>
+                    </div>
+                    <div className="nameNumero">
+                        <p> Urgences pour les sourds et malentendants </p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 
