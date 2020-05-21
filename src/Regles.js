4acbf3856f7ea97506c2259f55c4de7fb8c07611
LydieV <49896819+LydieV@users.noreply.github.com>
2020-05-21 12:33:32 +0200

    Début mainpage.

--- src/Regles.js
+++ src/Regles.js
@@ -16,6 +16,35 @@ export default function Regles() {
     return (
         <div>
             <Header page={'Règles '+pays}> </Header>
+            <div className="rules">
+                <div className="mainRules">
+                    <h2 className="titlePage"> Main rules </h2>
+                    <div className="liste">
+                        <div className="onerule">
+                            <img src={'/images/main.png'} alt={'Fermer le menu'}/>
+                            <p> Se laver très régulièrement les mains </p>
+                        </div>
+                        <div className="onerule">
+                            <img src={'/images/moucher.png'} alt={'Fermer le menu'}/>
+                            <p> Tousser ou éternuer dans son coude ou dans un mouchoir</p>
+                        </div>
+                        <div className="onerule">
+                            <img src={'/images/mouchoir.png'} alt={'Fermer le menu'}/>
+                            <p> Utiliser un mouchoir à usage unique et le jeter </p>
+                        </div>
+                        <div className="onerule">
+                            <img src={'/images/serrermains.png'} alt={'Fermer le menu'}/>
+                            <p> Saluer sans se serrer la main, éviter les embrassades </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="categories">
+                    <p> Sélectionner une catégorie </p>
+                    <div className="liste">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
