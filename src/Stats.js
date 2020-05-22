387eb7dc5b71c6f1f8ccb4193cf6535594916214
FlorianLz <florianlaignez@gmail.com>
2020-05-22 14:50:15 +0200

    Ajout proxy pour récupérer les données de l'api.

--- src/Stats.js
+++ src/Stats.js
@@ -8,17 +8,18 @@ export default function Stats() {
     const [cookies] = useCookies(['pays']);
     const [deaths,setDeaths] = useState('0');
     const [confirmed,setConfirmed] = useState('0');
+    const [loading, setLoading] = useState(true);
     let pays=cookies.pays;
     let codepays=cookies.codepays;
 
     async function getData(codepays){
-        const data = (await axios.get('https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code='+codepays)).data;
+        const data = (await axios.get('https://cors-anywhere.herokuapp.com/https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code='+codepays)).data;
         setConfirmed(data.latest.confirmed);
         setDeaths(data.latest.deaths);
+        setLoading(false);
     }
 
-    getData(codepays)
-
+    getData(codepays);
 
     if(!cookies.pays){
         return (
@@ -26,19 +27,43 @@ export default function Stats() {
         );
     }
 
-    return (
-        <div>
-            <Header page={'Statistiques '+pays}> </Header>
-            <div className="stats">
-                <div className="intro">
-                    <h2>Ici vous pouvez retrouver les statistiques du pays que vous avez sélectionné</h2>
+    if(loading === true){
+        return (
+            <div>
+                <Header page={'Statistiques '+pays}> </Header>
+                <div className="stats">
+                    <div className="intro">
+                        <h2>Ici vous pouvez retrouver les statistiques du pays que vous avez sélectionné</h2>
+                    </div>
+                    <div className="lds-roller">
+                        <div> </div>
+                        <div> </div>
+                        <div> </div>
+                        <div> </div>
+                        <div> </div>
+                        <div> </div>
+                        <div> </div>
+                        <div> </div>
+                    </div>
                 </div>
-                <p className={'pays'}>Pays sélectionné : {pays}</p>
-                <p>Nombre de cas : {confirmed}</p>
-                <p>Nombre de morts : {deaths}</p>
             </div>
-        </div>
 
-    );
+        );
+    }else{
+        return (
+            <div>
+                <Header page={'Statistiques '+pays}> </Header>
+                <div className="stats">
+                    <div className="intro">
+                        <h2>Ici vous pouvez retrouver les statistiques du pays que vous avez sélectionné</h2>
+                    </div>
+                    <p className={'pays'}>Pays sélectionné : {pays}</p>
+                    <p>Nombre de cas : {confirmed}</p>
+                    <p>Nombre de morts : {deaths}</p>
+                </div>
+            </div>
+
+        );
+    }
 
 }
