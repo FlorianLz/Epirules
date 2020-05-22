7e384c9bab11ad597294b51f63a6f30d9f198f80
FlorianLz <florianlaignez@gmail.com>
2020-05-22 12:32:40 +0200

    Modif header.

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,128 @@
-import React from 'react';
-import Menu from "./Menu";
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
 
 export default function Header(props) {
-    return (
+    const [visible,setVisible]=useState(false);
+
+    function changer() {
+        if(visible === false){
+            setVisible(true);
+        }else{
+            setVisible(false);
+        }
+    }
+
+    if(visible === true){
+        return (
         <div>
             <div className={'header'}>
                 <img src={'/images/epirules.png'} alt={'Epirules'}/>
-                <h3>{props.page}</h3>
             </div>
-            <Menu pays={props.pays}> </Menu>
+            <div className="header_titre">
+                <div className="vague">
+                    <img src={'/images/vague.png'} alt={'Vague'}/>
+                    <h2>{props.page}</h2>
+                </div>
+            </div>
+            <div className="menu">
+                <img src={'/images/menuouvert.png'} onClick={e=>changer(e)} alt={'Fermer le menu'}/>
+                <div className="liens">
+                    <Link to={'/regles'}>
+                        <div className="lien">
+                            <div className="titre">Accueil</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/urgences'}>
+                        <div className="lien">
+                            <div className="titre">Urgences</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/faq'}>
+                        <div className="lien">
+                            <div className="titre">FAQ</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/stats'}>
+                        <div className="lien">
+                            <div className="titre">Statistiques</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">Choix pays</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/a-propos'}>
+                        <div className="lien">
+                            <div className="titre">À propos de nous</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
 
-    );
+        );
+    }else{
+        return (
+        <div>
+            <div className={'header'}>
+                <img src={'/images/epirules.png'} alt={'Epirules'}/>
+            </div>
+            <div className="header_titre">
+                <div className="vague">
+                    <img src={'/images/vague.png'} alt={'Vague'} />
+                    <h2>{props.page}</h2>
+                </div>
+            </div>
+            <div className="menu ferme">
+                <img src={'/images/menuferme.png'} onClick={e=>changer(e)} alt={'Ouvrir le menu'}/>
+                <div className="liens">
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">Accueil</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">Urgences</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">FAQ</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">Statistiques</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">Choix pays</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                    <Link to={'/'}>
+                        <div className="lien">
+                            <div className="titre">À propos de nous</div>
+                            <i className="fas fa-arrow-right"> </i>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        );
+    }
 
 }
