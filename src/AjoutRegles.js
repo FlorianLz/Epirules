4cdb81fce6d1b7bcce8ad704cb70556b6af170c7
LydieV <49896819+LydieV@users.noreply.github.com>
2020-05-25 15:49:58 +0200

    Ajout d'une règle.

--- src/AjoutRegles.js
+++ src/AjoutRegles.js
@@ -53,7 +53,6 @@ export default function Regles() {
     function actualiserChoix(e) {
         let choix = e.target.options[e.target.selectedIndex].value;
         setChoisi(choix);
-        console.log(choix);
     }
 
     useEffect(()=>{
@@ -68,23 +67,41 @@ export default function Regles() {
 
     function ajoutRegle(e){
         e.preventDefault();
-        //recuperation diff champs
         let regle = document.querySelector("input[name='regle']").value;
         let radios = document.querySelectorAll("input[name='type']");
         let type = "";
+        let categorie = choisi;
 
+        //verif pour les boutons radios
         for(let i=0; i < radios.length; i++){
             if(radios[i].checked){
                 type = radios[i].value;
             }
         }
 
-        if(regle === '' || choisi === false || type === ''){
+        if(regle === '' || categorie === false || type === ''){
             document.querySelector('p.status_ok').innerHTML = '';
             document.querySelector('p.status').innerHTML = 'Merci de remplir tous les champs.';
         } else {
-            document.querySelector('p.status').innerHTML = '';
-            document.querySelector('p.status_ok').innerHTML = 'Votre règle a bien été ajouté !';
+            db.collection("regles").add({
+                desc: regle,
+                idpays: idpays,
+                namecategorie: categorie,
+                type: type
+            })
+                .then(function(docRef) {
+                    //console.log("Document written with ID: ", docRef.id);
+                    document.querySelector("form[name='formRegle']").reset();
+                    document.querySelector('p.status').innerHTML = '';
+                    document.querySelector('p.status_ok').innerHTML = 'Votre règle a bien été ajouté !';
+
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    document.querySelector('p.status').innerHTML = 'Erreur lors de l\'envoi de la règle';
+                    document.querySelector('p.status_ok').innerHTML = '';
+                });
+
         }
 
     }
@@ -93,30 +110,40 @@ export default function Regles() {
         return (
             <div>
                 <Header page={'Ajouter une règle '}> </Header>
-                <div className="ajouterRegle">
-                    <p> Pays sélectionné : {pays}</p>
-                    <form onSubmit={e => ajoutRegle(e)} id={"formRegle"}>
-                        <select id="ListeCategories" defaultValue={'choix'} onChange={e=>actualiserChoix(e)}>
-                            <option value="choix" name="choix" hidden>Choisissez la catégorie </option>
-                            {jsxListeCategories}
-                        </select>
+                <div className="ajouterRegles">
+                    <h3> Pays sélectionné : {pays}</h3>
+                    <form onSubmit={e => ajoutRegle(e)} name={"formRegle"} id={"formRegle"}>
+                        <div className="listeCategories">
+                            <select id="ListeCategories" defaultValue={'choix'} onChange={e=>actualiserChoix(e)}>
+                                <option value="choix" name="choix" hidden> Choisissez la catégorie </option>
+                                {jsxListeCategories}
+                            </select>
+                        </div>
                         <div className="selectType">
                             <h3> Sélectionner le type de règle </h3>
-                            <input type="radio" id="must" name="type" value="must"/>
-                            <label htmlFor="type"> Je dois </label>
-                            <input type="radio" id="mustnot" name="type" value="mustnot"/>
-                            <label htmlFor="type"> Je ne dois pas </label>
-                            <input type="radio" id="can" name="type" value="can"/>
-                            <label htmlFor="type"> Je peux </label>
+                            <div className="listeType">
+                                <div>
+                                    <input type="radio" id="must" name="type" value="must"/>
+                                    <label htmlFor="type"> Je dois </label>
+                                </div>
+                                <div>
+                                    <input type="radio" id="mustnot" name="type" value="mustnot"/>
+                                    <label htmlFor="type"> Je ne dois pas </label>
+                                </div>
+                                <div>
+                                    <input type="radio" id="can" name="type" value="can"/>
+                                    <label htmlFor="type"> Je peux </label>
+                                </div>
+                            </div>
                         </div>
                         <div>
                             <h3> Écrire la règle </h3>
-                            <input type="text" name="regle" placeholder="La règle..." id="desc"/>
+                            <input type="text" name="regle" placeholder="La règle..." id="desc" className="descRegle"/>
                         </div>
                         <div>
                             <p className="status"></p>
                             <p className="status_ok"></p>
-                            <input type="submit" value="Valider la règle"/>
+                            <input type="submit" value="Valider la règle" className="validateRule"/>
                         </div>
 
                     </form>
