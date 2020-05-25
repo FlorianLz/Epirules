826129c494d61af45978002189a53430d7f0ed62
LydieV <49896819+LydieV@users.noreply.github.com>
2020-05-25 12:40:02 +0200

    Verif règles pour ajout.

--- src/AjoutRegles.js
+++ src/AjoutRegles.js
@@ -68,6 +68,25 @@ export default function Regles() {
 
     function ajoutRegle(e){
         e.preventDefault();
+        //recuperation diff champs
+        let regle = document.querySelector("input[name='regle']").value;
+        let radios = document.querySelectorAll("input[name='type']");
+        let type = "";
+
+        for(let i=0; i < radios.length; i++){
+            if(radios[i].checked){
+                type = radios[i].value;
+            }
+        }
+
+        if(regle === '' || choisi === false || type === ''){
+            document.querySelector('p.status_ok').innerHTML = '';
+            document.querySelector('p.status').innerHTML = 'Merci de remplir tous les champs.';
+        } else {
+            document.querySelector('p.status').innerHTML = '';
+            document.querySelector('p.status_ok').innerHTML = 'Votre règle a bien été ajouté !';
+        }
+
     }
 
     if (loading === false){
@@ -78,12 +97,12 @@ export default function Regles() {
                     <p> Pays sélectionné : {pays}</p>
                     <form onSubmit={e => ajoutRegle(e)} id={"formRegle"}>
                         <select id="ListeCategories" defaultValue={'choix'} onChange={e=>actualiserChoix(e)}>
-                            <option value="choix" hidden>Choisissez la catégorie </option>
+                            <option value="choix" name="choix" hidden>Choisissez la catégorie </option>
                             {jsxListeCategories}
                         </select>
                         <div className="selectType">
                             <h3> Sélectionner le type de règle </h3>
-                            <input type="radio" id="must" name="type" value="must" />
+                            <input type="radio" id="must" name="type" value="must"/>
                             <label htmlFor="type"> Je dois </label>
                             <input type="radio" id="mustnot" name="type" value="mustnot"/>
                             <label htmlFor="type"> Je ne dois pas </label>
@@ -92,9 +111,11 @@ export default function Regles() {
                         </div>
                         <div>
                             <h3> Écrire la règle </h3>
-                            <input type="text" placeholder="La règle..." id="desc"/>
+                            <input type="text" name="regle" placeholder="La règle..." id="desc"/>
                         </div>
                         <div>
+                            <p className="status"></p>
+                            <p className="status_ok"></p>
                             <input type="submit" value="Valider la règle"/>
                         </div>
 
