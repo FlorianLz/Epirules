3fce701527f8da0feb69b5816533848b5a3ccd34
LydieV <49896819+LydieV@users.noreply.github.com>
2020-05-26 09:39:34 +0200

    Supprimer une catégorie.

--- src/ListeCategoriesAdd.js
+++ src/ListeCategoriesAdd.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function ListeCategoriesAdd(props) {
     return (
-        <p>{props.name}</p>
+        <div className="lineCategorie">
+            <p>{props.name}</p>
+            <div className="suppr">
+                <p><i className="fas fa-times" data-suppr={props.suppr} onClick={props.onSuppr}> </i> </p>
+            </div>
+        </div>
+
     );
 
 }
