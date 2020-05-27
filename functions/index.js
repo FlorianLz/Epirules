12278c864d1d2b1bbf5158315e596b9d9aa17379
FlorianLz <florianlaignez@gmail.com>
2020-05-27 17:40:45 +0200

    Ajout du template de l'email à envoyer lorsque l'on réponds à une question.

--- functions/index.js
+++ functions/index.js
@@ -37,8 +37,34 @@ var goMail = function (message,email,prenom,nom) {
         to: email, // list of receivers
         subject: 'Nous avons répondu à votre question !', // Subject line
         text: 'Ok !',
-        html: `<h3>Bonjour ${prenom} ${nom} !</h3><br /><p>Nous avons répondu à votre question : "${message}".</p>
-        <br /><p>Retrouvez la réponse sur l'appli :)</p>` // html body
+        html: `<div style="width:100%;height:100%;background-color:#1c1c1c;text-align: center;color:#fff;padding-bottom:40px;font-family: 'Arial', sans-serif;">
+        <div style="text-align: center; padding: 1rem 0;border-bottom: 6px solid #56AF32;width:95%;margin:0 auto;">
+            <img src="https://firebasestorage.googleapis.com/v0/b/epirules.appspot.com/o/epirules.png?alt=media&token=3108c152-8af3-423d-91df-c3b2f74aea46" alt="Epirules" style="width:20%;">
+        </div>
+        <p style="color:#fff;font-size:30px;">Merci de l'intéret que vous portez à notre application.</p>
+        <div>
+            <p style="font-size: 35px;color:#fff;text-align: left;border-bottom:6px solid #56AF32;width: max-content;padding-left:35px;">Rubrique FAQ :</p>
+            <div style="width:80%;margin:0 auto;color:#fff;text-align: left;font-size:20px;padding-top:30px;padding-bottom:60px;">
+                <p style="text-align:left">Bonjour ${prenom} ${nom},</p>
+                <p>Epirules a donné suite à votre question. Vous pouvez retrouver la réponse directement sur le site internet
+                    ou l’application. Pour cela, rendez-vous sur <a href="https://epirules.fr" style="text-decoration: none;color:#56AF32">ce lien</a> mais aussi directement sur l’App Store ou le Play
+                    Store.</p>
+                <p>Nous vous remercions une nouvelle fois de l’intérêt que vous portez à notre application et espérons
+                    qu’Epirules vous satisfasse amplement.
+                </p>
+                <p style="text-align: right">L'équipe Epirules</p>
+            </div>
+        </div>
+        <div style="width:40%;border-top:6px solid #56AF32;font-size:20px;text-align: left;padding-left:30px;">
+            <p style="font-style: italic">Un problème ? N’hésitez pas à nous contacter sur
+                <b><a style="text-decoration: none;color:#fff" href="mailto:epirules.contact@gmail.com" target="_blank">epirules.contact@gmail.com</a></b></p>
+            <div style="display: flex">
+                <p>Retrouvez-nous sur</p>
+                <a target="_blank" href="https://www.facebook.com/EpirulesOff"><img style="padding-left:20px;" width="65px" height="60px" src="https://firebasestorage.googleapis.com/v0/b/epirules.appspot.com/o/logo-FB.png?alt=media&token=13ce5594-31b0-4b16-9c94-0735d48330c5" alt="Facebook"></a>
+                <a target="_blank" href="https://twitter.com/EpirulesOff"><img style="padding-left:20px;" width="65px" height="60px" src="https://firebasestorage.googleapis.com/v0/b/epirules.appspot.com/o/Twitter_Bird.svg.png?alt=media&token=559b41ed-7eed-43ab-98a3-6173d3b153eb" alt="Twitter"></a>
+            </div>
+        </div>
+    </div>` // html body
     };
 
     //this is callback function to return status to firebase console
