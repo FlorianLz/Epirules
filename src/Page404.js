a9e1fe0dc98691477d919be81305bc5083aa7827
LydieV <49896819+LydieV@users.noreply.github.com>
2020-05-28 10:10:26 +0200

    Page 404.

--- src/Page404.js
+++ src/Page404.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from "./Header";
 
 
 export default function Page404() {
     return (
-        <div>
-            <h1>404</h1>
+        <div className="contenu404">
+            <Header page={''}> </Header>
+            <div className="erreur">
+                <h1> 404</h1>
+                <p> La page que vous recherchez est introuvable. </p>
+            </div>
         </div>
 
     );
